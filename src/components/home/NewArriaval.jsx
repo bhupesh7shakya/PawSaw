835b777b6ba9b3a228c98e073d7e5d9aaf2cf6b9
= <=>
2022-05-21 21:35:50 +0545

    creating template for the project

---
+++ src/components/home/NewArriaval.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class NewArriaval extends Component {
+  render() {
+    return (
+      <div>NewArriaval</div>
+    )
+  }
+}
+
+export default NewArriaval
---
+++ src/route/AppRoute.jsx
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from "react";
+import { Router,Route,Switch, Routes } from "react-router";
+import HomePage from "../pages/HomePage";
+class AppRoute extends Component {
+  render() {
+    return (
+      <Fragment>
+            <Routes>
+                <Route exact path="/" element={<HomePage/>} />
+            </Routes>
+      </Fragment>
+    );
+  }
+}
+
+export default AppRoute;
